9c5627616ebb06a9bb6291db9a3922a68d47bfcc
elprofa <76491497+elprofa@users.noreply.github.com>
2021-12-11 07:45:09 +0100

    update1

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,4 @@
 import Head from 'next/head'
-import Button from '../components/shared/Button'
-import Icon from '../components/shared/Icon'
-import Image from '../components/shared/Image'
-import Link from '../components/shared/Link'
-import SubTitle from '../components/shared/SubTitle'
-import Title from '../components/shared/Title'
-import { AiFillAndroid } from "react-icons/ai";
-import Input from '../components/shared/Input'
-import Select from '../components/shared/Select'
 import SectionAccueil from '../components/molecule/SectionAccueil'
 import SectionProduit from '../components/molecule/SectionProduit'
 export default function Home() {
